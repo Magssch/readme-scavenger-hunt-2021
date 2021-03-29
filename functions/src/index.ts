03481240d08315a50c76df46b926dc846aaa63e4
Magnus Schjølberg <magnus.schjolberg@gmail.com>
2021-03-30 00:25:51 +0200

    Add hidden paths for autogenerated codes with functions api

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,32 @@
 import * as functions from "firebase-functions";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const validCodes: {
+  [key: string]: string;
+} = {
+  X48V9B2U: "Du fant den første!",
+  WRLM548B: "Du fant den andre!",
+  TLK6P9DM: "Du fant den tredje!",
+  AHKGDHMC: "Du fant den fjerde!",
+  THP3UGP8: "Du fant den femte!",
+  QFAQFNNT: "Du fant den sjette!",
+  YK55RUCT: "Du fant den syvende!",
+  A2MLB722: "Du fant den åttende!",
+  S3URMC93: "Du fant den niende!",
+  K4X3M7PD: "Du fant den siste!",
+};
+
+export const checkCode = functions.https.onRequest(
+  async (request, response) => {
+    let code: string;
+    try {
+      code = JSON.parse(request.body).code;
+    } catch (e) {
+      code = request.body.code;
+    }
+    console.log(code);
+    if (!(code in validCodes)) {
+      response.sendStatus(404);
+    }
+    response.send(JSON.stringify({ message: validCodes[code] }));
+  }
+);
---
+++ src/CodeForwarder.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+
+interface CodeForwarderProps {
+  code: string;
+}
+
+const CodeForwarder: React.FC<CodeForwarderProps> = ({ code }) => {
+  const [apiResponse, setApiResponse] = useState<string>("");
+  const [requestSuccessful, setRequestSuccessful] = useState<boolean>(false);
+
+  const requestRiddle = (code: string) => {
+    fetch("./api/checkCode", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        code: code,
+      }),
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        setApiResponse(response.message);
+        setRequestSuccessful(true);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    requestRiddle(code);
+  }, []);
+
+  return (
+    <div>{requestSuccessful ? apiResponse : code + " is not a valid code"}</div>
+  );
+};
+
+export default CodeForwarder;
