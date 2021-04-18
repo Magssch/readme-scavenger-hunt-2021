b90a622097739b531cc41c744bf8713e03fe4a0b
Magnus Schjølberg <magnus.schjolberg@gmail.com>
2021-04-18 22:26:06 +0200

    Fix linting

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  Route,
-  BrowserRouter as Router,
-  Switch,
-  Redirect,
-} from "react-router-dom";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import CodeForwarder from "./CodeForwarder";
 import Start from "./Start";
 
