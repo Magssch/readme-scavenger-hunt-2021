dc5ae1a6f732ce5a21bd4fe9dcffa8d1ddf85828
Magnus Schjølberg <magnus.schjolberg@gmail.com>
2021-04-19 22:24:25 +0200

    Update CodeForwarder.tsx

--- src/CodeForwarder.tsx
+++ src/CodeForwarder.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
